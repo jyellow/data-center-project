d57b376f93d447104333bd5aba0c1f3737f8c8db
jyellow <johannyellow@outlook.com>
2022-10-09 20:36:30 +0800

    初始化项目

---
+++ src/main/java/me/jyellow/datacenter/DataCenterProjectApplication.java
@@ -0,0 +1,13 @@
+package me.jyellow.datacenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCenterProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataCenterProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/jyellow/datacenter/DataCenterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jyellow.datacenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCenterProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
